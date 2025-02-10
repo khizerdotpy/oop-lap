8d28d4cd1359aea510bf848e29f3c1b3a83ce751
khizer <khizer.sheikh05@gmail.com>
2025-02-10 12:25:43 +0500

    Create q5.cpp

---
+++ lab 4/q5.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee{
+
+    int employeeID;
+    string name;
+    string department;
+    double salary;
+
+    public:
+
+    Employee(){
+        salary=0.0;
+    }
+
+    Employee(int id=0,string n="joan",string dt="mech",double s=400.0):
+        employeeID(id),name(n),department(dt),salary(s){}
+
+    void giveBonus(double amt){
+        salary+=amt;
+        cout<<"\nggs, Bonus added\n";
+    }
+
+    bool isManager(){
+        if(department=="Management"){
+            cout<<"\nyep manager\n";
+            return 1;
+        } else {
+            cout<<"\nnot manager\n";
+            return 0;
+        }
+    }
+
+    void displayDetail(){
+        cout<<"\nEmployee Details\n";
+
+        cout<<"\nEmployee ID: \n"<<employeeID<<"\nName: \n"<<name
+            <<"\nDepartment: \n"<<department<<"\nSalary: "<<salary<<endl;
+    }
+};
+
+int main(){
+
+    Employee e1(2000,"oj simpson","Management",560000);
+    Employee e2(5000,"peter parker","teacher",40000);
+
+    cout<<"\nBefore bonus: \n";
+    e2.displayDetail();
+    e2.giveBonus(500.00);
+    cout<<"\nAfter bonus: \n";
+    e2.displayDetail();
+
+    e1.isManager();
+    e2.isManager();
+
+    return 0;
+}
